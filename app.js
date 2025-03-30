a120cf27a935ec7accd8d24a48b28daa23d0c41b
rcampux <r.camposdesign@gmail.com>
2025-03-30 17:32:09 -0300

    Function verificarChute

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-let titulo = document.querySelector('h1')
+/* let titulo = document.querySelector('h1')
 titulo.innerHTML = 'Jogo Do Número Secreto';
 
 let paragrafo = document.querySelector('p')
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; */
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+   return parseInt(Math.random() * 10 + 1);
+}
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número no intervalo de 1 a 10');
 
 function verificarChute() {
-    console.log('O botão foi clicado!')
-} 
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1','Acertou Miserávi!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto, Parabéns!!!');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', `O número secreto é menor que <strong>${chute}</strong>`);
+        } else {
+            exibirTextoNaTela('p', `O número secreto é maior que <strong>${chute}</strong>`);
+        }
+    }
+ }
